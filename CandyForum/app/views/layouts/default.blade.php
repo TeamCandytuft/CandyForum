1ca49480921fe9374be59e2c5b496312742b041b
Vladislav Hadzhiyski <vladislav_hadzhiyski@hotmail.com>
2014-08-24 14:38:33 +0300

    master page created

--- CandyForum/app/views/layouts/default.blade.php
+++ CandyForum/app/views/layouts/default.blade.php
@@ -10,14 +10,36 @@
     <script src="{{ asset('./js/bootstrap.min.js') }}"></script>
 </head>
 <body>
-<header>
-    <h1>Candy forum Header</h1>
-</header>
-<main>
-    @yield('content')
-</main>
-<footer>
-    <h2>Candy Forum Footer</h2>
-</footer>
+    <div class="container">
+        <header class="row">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand logo" href="/">Candy Forum</a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="">test1</a></li>
+                            <li><a href="">test2</a></li>
+                            <li><a href="">test3</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    </div>
+    <div class="container main-content">
+        <main class="row">
+            @yield('content')
+        </main>
+        <div class="row">
+            <h2>Candy Forum footer</h2>
+        </div>
+    </div>
 </body>
 </html>
